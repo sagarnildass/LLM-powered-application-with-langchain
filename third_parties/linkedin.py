e84fe51e6cd12cbfc149185de00d50d847a0f3e7
Sagarnil Das <sagarnildass@gmail.com>
2024-03-26 18:02:56 +0530

    restructured folders

---
+++ third_parties/linkedin.py
@@ -0,0 +1,12 @@
+import os
+import requests
+from linkedin_scraper import Person, actions
+from selenium import webdriver
+
+def scrape_linkedin_profile(linkedin_profile_url:str):
+    """
+    Scrapes a linkedin profile for information
+    Manually scrape the information from the LinkedIn Profile
+    """
+    
+    pass
